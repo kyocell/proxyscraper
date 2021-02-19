92a477b5c243f5d88110c830d0e15ca96bc1ae9d
Kyocell <45465170+kyocell@users.noreply.github.com>
2021-02-19 18:21:30 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+
+# URLs being scraped -
+
+url = "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt"
+url2 = "https://proxy-daily.com/"
+url3 = "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt"
+
+# Driver -
+
+driver = webdriver.Chrome()
+
+
+# Source 1 - HTTP
+
+print("Scraping source 1...")
+driver.get(url)
+src1_elementtext = driver.find_element_by_xpath('/html/body/pre')
+src1_save = open("https.txt", "a")
+src1_save.write(src1_elementtext.text)
+src1_save.write("\n")
+src1_save.close()
+print("Done")
+
+
+
+# Source 2 - HTTP/S/SSL,Socks :
+
+print("Scraping source 2...")
+driver.get(url2)
+
+
+src2_http = driver.find_element_by_xpath("/html/body/div[3]/div/div[3]/div[2]")
+src2_http_save = open("https.txt", "a")
+src2_http_save.write(src2_http.text)
+src2_http_save.write("\n")
+src2_http_save.close()
+
+
+src2_socks4 = driver.find_element_by_xpath("/html/body/div[3]/div/div[3]/div[4]")
+src2_socks4_save = open("socks4.txt", "a")
+src2_socks4_save.write(src2_socks4.text)
+src2_socks4_save.write("\n")
+src2_socks4_save.close()
+
+
+src2_socks5 = driver.find_element_by_xpath("/html/body/div[3]/div/div[3]/div[6]")
+src2_socks5_save = open("socks5.txt", "a")
+src2_socks5_save.write(src2_socks5.text)
+src2_socks5_save.write("\n")
+src2_socks5_save.close()
+
+print("Done")
+
+
+
+# Source 3 - HTTP/S/SSL,Socks :
+
+print("Scraping source 3...")
+driver.get(url3)
+src3_socks5 = driver.find_element_by_xpath("/html/body/pre")
+src3_socks5_save = open("socks5.txt", 'a')
+src3_socks5_save.write(src3_socks5.text)
+src3_socks5_save.write("\n")
+src3_socks5_save.close()
+
+print("Done")
